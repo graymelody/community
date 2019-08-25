f55bbb2d2f95ff9f919fbdb27b7a264652ea0521
gray <graymelody@qq.com>
2019-08-25 17:48:37 +0800

    first commit

---
+++ src/main/java/xyz/gray/community/controller/IndexController.java
@@ -0,0 +1,11 @@
+package xyz.gray.community.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Gray on 2019-08-25 下午 02:50
+ */
+@Controller
+public class IndexController {
+
+}

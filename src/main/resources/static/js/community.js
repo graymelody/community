6e6a7f1cf0505db2893dcb6aba2d9ef98e70b044
gray <graymelody@qq.com>
2019-10-06 09:35:14 +0800

    不刷新页面登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -11,7 +11,17 @@ function submitComment() {
             "content":content
         }),
         success:function (result) {
-            console.log(result);
+            if (result.code === 200) {
+                $(".comment").hide();
+            } else if (result.code === 2003) {
+                var dowhat = confirm(result.message);
+                if (dowhat) {
+                    window.open("https://github.com/login/oauth/authorize?client_id=9aeb86d8b6e63deff761&redirect_uri=http://localhost:8080/callback&scope=user&state=1");
+                    window.localStorage.setItem("closable","true");
+                }
+            } else {
+                alert(result.message)
+            }
         },
         dataType:"json"}
     );
